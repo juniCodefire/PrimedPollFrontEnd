6a5ae52b1950e1647bcf94a9e95d8a6d573cd34f
Mr-Juni <junipreach2017@gmail.com>
2019-08-18 11:09:05 +0100

    update

--- public/Assets/js/customjs/forgotPassword.js
+++ public/Assets/js/customjs/forgotPassword.js
@@ -13,20 +13,20 @@ const status = (response) => {
     return response.json();
 }
 const processApiResponse = (data, email = "", errorText = "") => {
-    if(!data.ok) {
+    if (!data.ok) {
         let text;
-        switch(JSON.stringify(statusCode)) {
+        switch (JSON.stringify(statusCode)) {
             case "422":
                 text = `Email filed cannot be empty!`;
                 break;
             case "404":
-                text = `Email not Found in our system!`;
+                text = `Email not found in our database, please do check the mail and try again!`;
                 break;
             case "500":
                 text = `Email not sent, please try again!`;
                 break;
             default:
-              customAlert(`An email has been sent to ${email}, follow link to reset password!`);
+                customAlert(`An email has been sent to ${email}, follow link to reset password!`);
         }
         errorText.innerHTML = text;
         console.log(data);
@@ -47,21 +47,23 @@ const sendRecoverCode = (event) => {
     console.log(errorText);
     // email == ""? retrun 
     console.log(email);
-    const data = {email};
+    const data = {
+        email
+    };
     fetch(url, {
-        method: "POST",
-        mode: "cors",
-        headers: {
-            "Content-Type": "application/json"
-          },
-        body: JSON.stringify(data)
-    })
-    .then(response => status(response)) 
-    .then(data => processApiResponse(data, email, errorText))
-    .catch(error => {
-        console.log(error)
-    });
-    
+            method: "POST",
+            mode: "cors",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        })
+        .then(response => status(response))
+        .then(data => processApiResponse(data, email, errorText))
+        .catch(error => {
+            console.log(error)
+        });
+
 }
 
 forgotpassForm.addEventListener('submit', (event) => sendRecoverCode(event))
---
+++ public/Assets/js/customjs/reset_password.js
@@ -0,0 +1,80 @@
+//Api To Send Recovery Code to Email Aftre Email Check
+//Api To Reset The password Code
+//Get The token form the url 
+const resetpassForm = document.querySelector("#resetPass-form");
+let statusCode;
+const parsedUrl = new URL(window.location.href);
+const getSearchParam = parsedUrl.searchParams;
+//Get Permission 
+const customAlert = (value) => {
+    return swal(value);
+}
+let verifycode = getSearchParam.get("verifycode");
+if (verifycode == '' || verifycode.length < 5) {
+    customAlert(`Verify code is empty! or have an invalid character length`);
+    setTimeout(() => {
+        location.href = `${ window.location.origin }/Users/signin.html`;
+    }, 5000);
+}
+//Handle all fetch Api Error Here
+const status = (response) => {
+    statusCode = response.status;
+    return response.json();
+}
+const processApiResponse = (data, errorText = "") => {
+    if (!data.ok) {
+        let text;
+        switch (JSON.stringify(statusCode)) {
+            case "422":
+                text = `Password filed cannot be empty!`;
+                break;
+            case "401":
+                customAlert(`Error reseting password, please follow the link from your email to get valid verification code!`);
+                break;
+            case "500":
+                customAlert(`Error changing password, please refresh and try aagain!`);
+                break;
+            default:
+                customAlert(`New password created successfully!`);
+                setTimeout(() => {
+                    location.href = `${ window.location.origin }/Users/signin.html`;
+                }, 5000);
+        }
+        errorText.innerHTML = text;
+    }
+}
+const sendRecoverCode = (event) => {
+    event.preventDefault();
+    //Send to the email check and Send Code Api
+    const url = `${baseUrl}api/password/change`;
+    const password = event.target[0].value;
+    const password_confirmation = event.target[1].value;
+    const errorText = event.target.children[1].children[1].children[1];
+
+    errorText.innerHTML = "";
+    if (password == "") {
+        return errorText.innerHTML = `Password filed cannot be empty!`;
+    }
+
+    const data = {
+        password,
+        password_confirmation,
+        verifycode
+    };
+    fetch(url, {
+            method: "PUT",
+            mode: "cors",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        })
+        .then(response => status(response))
+        .then(data => processApiResponse(data, errorText))
+        .catch(error => {
+            customAlert(`An error occured, this may be from an insufficient network coverage, please refresh and try again!`);
+        });
+
+}
+
+resetpassForm.addEventListener('submit', (event) => sendRecoverCode(event))
