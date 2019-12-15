cbd2e077451d1b11da44c8b12cf3597feb039b93
unknown <junicodefire@gmail.com>
2019-12-15 22:41:42 +0100

    fully fixed

--- public/primed_assets/js/customjs/domainHandler.js
+++ public/primed_assets/js/customjs/domainHandler.js
@@ -1,5 +1,5 @@
 const dispatch = (url, default_link) => {
-	if(location.origin == 'primedpoll.com' ||  location.origin == 'www.primedpoll.com' ||  location.origin == 'app.primedpoll.com'){
+	if(location.origin === 'https://primedpoll.com' ||  location.origin === 'https://www.primedpoll.com'){
 		location.replace(url)
 	}else {
 	    location.replace(default_link)
@@ -9,22 +9,22 @@ const authIn = (command) => {
 
 	switch (command) {
 		case 'login':
-			dispatch('app.primedpoll.com/user/signin.html', '/user/signin.html');
+			dispatch('https://app.primedpoll.com/user/signin.html', '/user/signin.html');
 		break;
 		case 'signup':
-			dispatch('app.primedpoll.com/user/signup.html', '/user/signup.html');
+			dispatch('https://app.primedpoll.com/user/signup.html', '/user/signup.html');
 		break;
 		case 'forgot':
-			dispatch('app.primedpoll.com/user/forget_password.html', '/user/forget_password.html');
+			dispatch('https://app.primedpoll.com/user/forget_password.html', '/user/forget_password.html');
 		break;
 		case 'reset':
-			dispatch('app.primedpoll.com/user/reset_password.html', '/user/reset_password.html');
+			dispatch('https://app.primedpoll.com/user/reset_password.html', '/user/reset_password.html');
 		break;
 		case 'confirm':
-			dispatch('app.primedpoll.com/user/confirmation-registration.html', '/user/confirmation-registration.html');
+			dispatch('https://app.primedpoll.com/user/confirmation-registration.html', '/user/confirmation-registration.html');
 		break;
 		case 'complete':
-			dispatch('app.primedpoll.com/user/signup.html', '/user/signup.html');
+			dispatch('https://app.primedpoll.com/user/signup.html', '/user/signup.html');
 		break;
 	}
 }
